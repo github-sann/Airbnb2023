e2fa567514c1e1df0796a8d641d1b5c81a64e062
github-sann <152688587+github-sann@users.noreply.github.com>
2023-12-08 12:06:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -102,9 +102,9 @@ app.use((req,res,next)=>{
 
 
 
-
-app.use("/", listings);
-app.use("/:id/reviews", reviews); 
+app.use("/",listings);
+app.use("/listings", listings);
+app.use("listings/:id/reviews", reviews); 
 app.use("/",users);
 
 app.all("*",(req,res,next)=>{
